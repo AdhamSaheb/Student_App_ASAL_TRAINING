fa76ff0af61428e0a9de44605b044a0539f11752
Adham Saheb <asaheb@asaltech.com>
2021-06-09 09:30:05 +0300

    pre-DTO

--- Frontend/student-app/src/app/Components/Student-Components/student-list/student-list.component.ts
+++ Frontend/student-app/src/app/Components/Student-Components/student-list/student-list.component.ts
@@ -3,6 +3,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { Student } from 'src/app/Models/Student.model';
 import { StudentService } from 'src/app/Services/Facade_Services/student-service.service';
+import { ConfirmationModalComponent } from '../../Courses-Components/confirmation-modal/confirmation-modal.component';
 import { StudentModalComponent } from '../student-modal/student-modal.component';
 
 @Component({
@@ -22,22 +23,27 @@ export class StudentListComponent implements OnInit {
       this._studentService.getStudents().subscribe((studentsLoaded)=> {
       this.students =studentsLoaded ; 
     });
-    this._studentService.getLoading().subscribe((isLoading) => {
+      this._studentService.getLoading().subscribe((isLoading) => {
       this.isLoading= isLoading ;
     } ) ;
   }
 
   onDeleteStudentClicked(student :  Student) {
-    this._studentService.removeStudent(student).subscribe((successState)=> {
-      //do something when removed, a toast maybe
-    }) ;
+    const modalRef = this.modalService.open(ConfirmationModalComponent) ;
+    modalRef.componentInstance.title = "Are you sure ? " ; 
+    modalRef.componentInstance.message = "Are you sure you want to delete " + student.firstName  + "?" ;
+    modalRef.result.then((res) => {
+        this._studentService.removeStudent(student).subscribe((successState)=> {
+        this.toastr.success("Student Removed");
+      });
+    });
   }
 
   openCreateStudentModal() {
     const modalRef = this.modalService.open(StudentModalComponent, {backdrop: 'static'});
     modalRef.componentInstance.modalTitle = 'Create a new Student' ;
     modalRef.result.then((student)=> {
-      this._studentService.addStudent(student).subscribe(()=> {
+        this._studentService.addStudent(student).subscribe(()=> {
         this.toastr.success('User Created Successfully');
       });
     });
@@ -48,7 +54,7 @@ export class StudentListComponent implements OnInit {
     modalRef.componentInstance.modalTitle = 'Edit ' +  student.firstName ;
     modalRef.componentInstance.inputStudent = student ;
     modalRef.result.then((student)=> {
-      this._studentService.updateStudent(student).subscribe(()=> {
+        this._studentService.updateStudent(student).subscribe(()=> {
         this.toastr.success('User Updated Successfully');
       });
     });
